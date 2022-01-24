527ac1cda9fecb70e26ca519e6a45c1e1a97e21c
Noah <ntidwell34@yahoo.com>
2022-01-24 13:02:30 -0600

    type Definitions for User, Story, and Comment

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    stories: [Story]
+    comments: [Comment]
+}
+
+type Story {
+    _id: ID
+    storyText: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+}
+
+type Comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    stories(username: String!): [Story]
+    thought(_id: ID!): Story
+
+}
+
+type Mutation {
+    login(username: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addStory(storyText: String!): Story
+    addComment(storyId: ID!, commentBody: String!): Story
+}
+`;
