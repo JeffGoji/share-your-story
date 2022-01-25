dfdbbd17d4f5b9e9755b38ec90ebe692f1e47446
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-01-24 18:12:01 -0600

    cleaned up server.js added models, cleaned config - now server compiles

---
+++ server/models/Story.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+const commentSchema = require("./Comment");
+const dateFormat = require("../utils/dateFormat");
+
+const storySchema = new Schema(
+  {
+    storyText: {
+      type: String,
+      required: "Please leave a story",
+      minlength: 1,
+      maxlength: 500,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    comments: [commentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+storySchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
+
+const Story = model("Story", storySchema);
+
+module.exports = Story;
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require("./User");
+const Story = require("./Story");
+
+module.exports = { User, Story };
